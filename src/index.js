55cba60a15d13e04afee3b1525fad656c75faae4
Geovani José <geovani-lol@hotmail.com>
2019-08-09 02:31:20 -0300

    Add style page Login

--- src/index.js
+++ src/index.js
@@ -1,34 +1,9 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
 
-const App = () => {
+import Login from './pages/Login'
+
+export default function App () {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello Jessica</Text>
-    </View>
+    <Login />
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#7159c1',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  text: {
-    fontWeight: 'bold',
-    color: '#fff',
-    fontSize: 20
-  }
-})
-
-export default App
